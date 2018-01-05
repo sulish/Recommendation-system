f10d5c6a3e432b539a4533e0389331a549a458bc
culish <arcesz_neat@yahoo.co.id>
2018-01-05 17:33:59 +0700

    Nambah balajar

--- 2.py
+++ 2.py
@@ -56,7 +56,7 @@ with open('coba.csv', 'r') as f:
       output.close()
       i+=1
 
-# with open('hasil cluster.csv', 'a') as m: 
+# with open('hasil cluster.csv', 'a') as m:  wong blajar
 #     m.write(",".join(aaaClass))
 #       print row+','+a_list[i]
 #       i+=1
