a9a2bdefe49abc7681450e1305293ff8fe4624dd
Sulis Setiowati <st23.sulis@gmail.com>
2018-03-11 05:42:42 +0700

    Update epsdbscan.py

--- epsdbscan.py
+++ epsdbscan.py
@@ -2,7 +2,7 @@ import csv
 import pandas as pd
 import numpy as np 
 import folium
-#from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt 
 
 
@@ -14,10 +14,10 @@ coords = df.as_matrix(columns=['Latitude','Longitude'])
 #plot koordinat lokasi dari dataset
 plt.figure(figsize=(12,12))		#mengatur ukuran tampilan plot
 
-#m=Basemap(projection='merc',resolution='l',epsg=4269, llcrnrlon=-122.7, llcrnrlat=36.2, urcrnrlon= -120.8, urcrnrlat=37.5)
-#x,y = m(coords[:,1], coords[:,0])
-#m.scatter(x,y,5,marker='o', color='b')
-#m.arcgisimage(service = 'World_Shaded_Relief' ,xpixels=5000, verbose=False)
+m=Basemap(projection='merc',resolution='l',epsg=4269, llcrnrlon=-122.7, llcrnrlat=36.2, urcrnrlon= -120.8, urcrnrlat=37.5)
+x,y = m(coords[:,1], coords[:,0])
+m.scatter(x,y,5,marker='o', color='b')
+m.arcgisimage(service = 'World_Shaded_Relief' ,xpixels=5000, verbose=False)
 plt.show()
 
 #insisialisasi awal
@@ -26,11 +26,11 @@ import matplotlib.colors as colors
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
 
-kms_per_radian = 6371.0088			#konstanta untuk radius epsilon
-epsilon = 1/kms_per_radian
-
 #DBSCAN dengan sklearn
-db=DBSCAN(eps=epsilon, min_samples=5, algorithm='ball_tree',metric='haversine').fit(np.radians(coords))
+kms_per_radian = 6371.0088
+epsilon = 0.30 / kms_per_radian
+start_time = time.time()
+db = DBSCAN(eps=epsilon, min_samples=500, algorithm='ball_tree', metric='haversine').fit(np.radians(coords))
 
 cluster_labels = db.labels_
 n_clusters = len(set(cluster_labels))
@@ -51,7 +51,7 @@ def get_cmap(N):
 
 #untuk plot hasil akhir clustering
 plt.figure(figsize=(12,12))
-#m=Basemap(projection='merc',resolution='l',epsg=4269, llcrnrlon=-122.7, llcrnrlat=36.2, urcrnrlon= -120.8, urcrnrlat=37.5)
+m=Basemap(projection='merc',resolution='l',epsg=4269, llcrnrlon=-122.7, llcrnrlat=36.2, urcrnrlon= -120.8, urcrnrlat=37.5)
 
 unique_label=np.unique(cluster_labels)
 
@@ -63,7 +63,7 @@ for i, clus in enumerate(clusters):
 	lons_select = cluster[:,1]
 	lats_select = cluster[:,0]
 	x,y = m(lons_select,lats_select)
-	#m.scatter(x,y,5,marker='o',color=cmaps(i),zorder=10)
+	m.scatter(x,y,5,marker='o',color=cmaps(i),zorder=10)
 
-#m.arcgisimage(service='World_Shaded_Relief', xpixels=5000,verbose=False)
+m.arcgisimage(service='World_Shaded_Relief', xpixels=5000,verbose=False)
 plt.show()
